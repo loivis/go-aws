e83701a58efa8eea45a6254afaaeda4b72277e53
loivis <loivis@live.com>
2017-05-21 18:06:59 +0200

    delete ecr images

--- ecr/images/main.go
+++ ecr/images/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"log"
 	"sort"
+	"strings"
 
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/ecr"
 )
+
+const keep = 500
 
 var sess = session.Must(session.NewSession(&aws.Config{Region: aws.String("eu-west-1")}))
 var svc = ecr.New(sess)
@@ -26,15 +29,61 @@ func main() {
 	}
 	sort.Sort(sort.StringSlice(repositories))
 	log.Println("repositories:", repositories)
+	// repositories = []string{"portal-app"}
 	for _, repository := range repositories {
-		log.Println(repository)
+		log.Println(strings.Repeat("=", 10), repository)
+		// log.Println(repository)
 		deleteUntaggedImages(repository)
+		deleteOldImages(repository)
+	}
+}
+
+func deleteOldImages(repo string) {
+	log.Println("get images except latest", keep)
+	// imageDetails := make([]ecr.ImageDetail, 0)
+	paramsDII := &ecr.DescribeImagesInput{
+		RepositoryName: aws.String(repo),
+	}
+	respDII, err := svc.DescribeImages(paramsDII)
+	imageDetails := respDII.ImageDetails
+	if err != nil {
+		log.Println(err.Error())
+	}
+	for respDII.NextToken != nil {
+		paramsDII = &ecr.DescribeImagesInput{
+			RepositoryName: aws.String(repo),
+			NextToken:      respDII.NextToken,
+		}
+		respDII, err = svc.DescribeImages(paramsDII)
+		imageDetails = append(imageDetails, respDII.ImageDetails...)
+	}
+	log.Println("total number of images:", len(imageDetails))
+	sort.SliceStable(imageDetails, func(i, j int) bool {
+		return imageDetails[i].ImagePushedAt.Unix() > imageDetails[j].ImagePushedAt.Unix()
+	})
+	if len(imageDetails) <= keep {
+		log.Println("image number under threshold")
+	} else {
+		log.Println("delete old images")
+		imageIds := make([]*ecr.ImageIdentifier, 0)
+		for _, imageDetail := range imageDetails[keep:] {
+			log.Println(*imageDetail.ImagePushedAt)
+			imageID := &ecr.ImageIdentifier{ImageDigest: imageDetail.ImageDigest, ImageTag: imageDetail.ImageTags[0]}
+			imageIds = append(imageIds, imageID)
+		}
+		paramsBDI := &ecr.BatchDeleteImageInput{
+			ImageIds:       imageIds,
+			RepositoryName: aws.String(repo),
+		}
+		_, err = svc.BatchDeleteImage(paramsBDI)
+		if err != nil {
+			log.Println(err.Error())
+		}
 	}
 }
 
 func deleteUntaggedImages(repo string) {
 	log.Println("get untagged images")
-	// imageIds := make([]string, 0)
 	paramsLI := &ecr.ListImagesInput{
 		RepositoryName: aws.String(repo),
 		Filter: &ecr.ListImagesFilter{
@@ -45,8 +94,8 @@ func deleteUntaggedImages(repo string) {
 	if err != nil {
 		log.Println(err.Error())
 	}
-	log.Println(respLI.ImageIds)
 	if len(respLI.ImageIds) > 0 {
+		log.Println(respLI.ImageIds)
 		log.Println("delete untagged images")
 		paramsBDI := &ecr.BatchDeleteImageInput{
 			ImageIds:       respLI.ImageIds,
@@ -57,5 +106,7 @@ func deleteUntaggedImages(repo string) {
 			log.Println(err.Error())
 		}
 		log.Println(respBDI)
+	} else {
+		log.Println("all images tagged")
 	}
 }
